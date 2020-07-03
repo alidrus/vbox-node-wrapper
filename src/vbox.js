7954faa8625cd282b4ac87f389350934528a8e8a
Abu Bakar Al-Idrus <alidrus@gmail.com>
2020-07-04 03:37:35 +0800

    Added usage display and ls command.

--- src/vbox.js
+++ src/vbox.js
@@ -2,16 +2,41 @@
  * vim: syntax=javascript expandtab tabstop=4 shiftwidth=4 softtabstop=4:
  */
 
-const path = require("path");
-const process = require("process");
-const config = require("./config.js");
-const commands = require("./commands.js");
+const path          = require("path"),
+      process       = require("process"),
+      { titlecase } = require("stringcase");
 
-console.dir(config);
+const commands = require("./commands.js"),
+      config   = require("./config.js")
+      package  = require("../package.json"),
+      ls       = require("./ls");
 
+// Process command line arguments and get only the basename
 let cliArguments = [];
 process.argv.forEach((argv) => {
     cliArguments.push(path.basename(argv));
 });
 
-console.dir(cliArguments);
+if (process.argv.length < 3)
+{
+    // Show package information
+    console.log(titlecase(package.name) + " " + package.version + "\n"
+        + package.description + "\n");
+
+    // Show usage information.
+    console.log("Usage:");
+    console.log("    " + path.basename(process.argv[0]) + " <command> [options [arguments]]\n");
+
+    // Show available commands.
+    console.log("Available commands:");
+    console.log("    ls    List out available VMs");
+
+    process.exit(0);
+}
+
+// VM Listing
+if (process.argv[2] == "ls")
+{
+    console.log(ls(config));
+    process.exit(0);
+}
