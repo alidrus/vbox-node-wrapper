fc671ace5e6ad2f9fe1225465aad1a9466e4ea26
Abu Bakar Al-Idrus <alidrus@gmail.com>
2020-07-04 03:43:05 +0800

    Format listing with a table. Added VM state. Update configuration while looping through entries.

--- src/ls.js
+++ src/ls.js
@@ -2,23 +2,27 @@
  * vim: syntax=javascript expandtab tabstop=4 shiftwidth=4 softtabstop=4:
  */
 
+const Table = require("tty-table");
 const commands = require("./commands.js");
 
 module.exports = (config) => {
     let vmList = [];
 
-    commands.ls().split(/[\r\n]+/g).forEach((vmInfo) => {
-        const matches = vmInfo.match(/^"([^"]+)" {([^}]+)}/);
+    commands.ls().split(/[\r\n]+/g).forEach((entry, index) => {
+        const matches = entry.match(/^"([^"]+)" {([^}]+)}/);
 
         if (matches === null)
         {
-            console.error("Error: Unable to parse VM list.");
+            console.error("Error: Unable to parse VM list.\n'%s'", entry);
             process.exit(1);
         }
 
+        const name = matches[1],
+              uuid = matches[2];
+
         let vmConfig;
 
-        if (Object.keys(config.config).find(vm => vm === matches[2]))
+        if (config.config.find(vm => vm.uuid === uuid))
         {
             vmConfig = config.config[matches[2]];
         }
@@ -27,21 +31,53 @@ module.exports = (config) => {
             vmConfig = {};
         }
 
-        vmList.push({
-            name: matches[1],
-            uuid: matches[2],
-            config: vmConfig
+        let state = null;
+        commands.showvminfo(uuid).split(/[\r\n]+/g).forEach((vm) => {
+            const matches = vm.match(/^VMState=\"([^=]+)\"$/);
+            if (matches !== null)
+            {
+                state = matches[1];
+            }
         });
+
+        vmList.push({
+            vmNum: index.toString(10),
+            name,
+            uuid,
+            state
+        });
+
+        config.update(uuid, name);
     });
 
-    let t = new Table;
+    const options = {
+        borderColor: "green",
+        borderStyle: "solid",
+        color:       "white",
+        align:       "left"
+    };
 
-    vmList.forEach((vm, index) => {
-        t.cell('VM#', index);
-        t.cell('VM Name', vm.name);
-        t.cell('UUID', vm.uuid);
-        t.newRow();
-    });
+    const header = [{
+        value: "vmNum",
+        alias: "VM#",
+        align: "left",
+        headerAlign: "left"
+    }, {
+        value: "name",
+        alias: "VM Name",
+        align: "left",
+        headerAlign: "left"
+    }, {
+        value: "uuid",
+        alias: "UUID",
+        align: "left",
+        headerAlign: "left"
+    }, {
+        value: "state",
+        alias: "State",
+        align: "left",
+        headerAlign: "left"
+    }];
 
-    return t.toString();
+    return "   Virtual Machine Listing" + Table(header, vmList, options).render();
 };
