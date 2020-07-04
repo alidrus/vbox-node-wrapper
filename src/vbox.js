d92ad6dfa1c895601c3a8d68732819e7200e3c73
Abu Bakar Al-Idrus <alidrus@gmail.com>
2020-07-05 04:45:04 +0800

    Added set, start, ssh, suspend and shutdown commands.

--- src/vbox.js
+++ src/vbox.js
@@ -8,7 +8,9 @@ const path          = require("path"),
 const commands = require("./commands.js"),
       config   = require("./config.js")
       help     = require("./help.js"),
-      ls       = require("./ls.js");
+      ls       = require("./ls.js")
+      ssh      = require("./ssh.js"),
+      set      = require("./set.js");
 
 // Process command line arguments and get only the basename
 let cliArguments = [];
@@ -16,15 +18,15 @@ process.argv.forEach((argv) => {
     cliArguments.push(path.basename(argv));
 });
 
+// Show help
 if (process.argv.length <= 2 || (process.argv.length >= 3 && process.argv[2] === "help"))
 {
     let parameters = Array.from(process.argv);
 
-    parameters.shift();
-    parameters.shift();
-    parameters.shift();
+    parameters.splice(0, 3);
 
     help(process.argv, parameters);
+
     process.exit(0);
 }
 
@@ -32,5 +34,120 @@ if (process.argv.length <= 2 || (process.argv.length >= 3 && process.argv[2] ===
 if (process.argv[2] == "ls")
 {
     console.log(ls(config));
-    process.exit(0);
+}
+
+// Set VM properties
+if (process.argv.length >= 3 && process.argv[2] == "set") {
+    if (process.argv.length >= 4 && process.argv[3].match(/^[0-9]+$/g))
+    {
+        const vmNum = parseInt(process.argv[3], 10);
+
+        if (config.config[vmNum])
+        {
+            let parameters = Array.from(process.argv);
+
+            parameters.splice(0, 4);
+
+            if (parameters[0])
+            {
+                set(config.config[vmNum], ...parameters);
+            }
+            else
+            {
+                help(process.argv, ["set"]);
+            }
+        }
+    }
+    else
+    {
+        help(process.argv, ["set"]);
+    }
+}
+
+// Start VM
+if (process.argv.length >= 3 && process.argv[2] == "start")
+{
+    if (process.argv.length >= 4 && process.argv[3].match(/^[0-9]+$/g))
+    {
+        const vmNum = parseInt(process.argv[3], 10);
+
+        if (config.config[vmNum])
+        {
+            const start = commands.start(config.config[vmNum].uuid).trim();
+            if (start)
+            {
+                console.log(start);
+            }
+        }
+    }
+    else
+    {
+        help(process.argv, ["start"]);
+    }
+}
+
+// SSH VM
+if (process.argv.length >= 3 && process.argv[2] == "ssh")
+{
+    if (process.argv.length >= 5 && process.argv[3].match(/^[0-9]+$/g) && process.argv[4])
+    {
+        const vmNum    = parseInt(process.argv[3], 10),
+              username = process.argv[4];
+
+        if (config.config[vmNum])
+        {
+            ssh(config.config[vmNum], username);
+        }
+    }
+    else
+    {
+        help(process.argv, ["ssh"]);
+    }
+}
+
+
+// Suspend VM
+if (process.argv.length >= 3 && process.argv[2] == "suspend")
+{
+    if (process.argv.length >= 4 && process.argv[3].match(/^[0-9]+$/g))
+    {
+        const vmNum = parseInt(process.argv[3], 10);
+
+        if (config.config[vmNum])
+        {
+            const suspend = commands.suspend(config.config[vmNum].uuid).trim();
+
+            if (suspend)
+            {
+                console.log(suspend);
+            }
+        }
+    }
+    else
+    {
+        help(process.argv, ["suspend"]);
+    }
+}
+
+// Shutdown VM
+if (process.argv.length >= 3 && process.argv[2] == "shutdown")
+{
+    if (process.argv.length >= 4 && process.argv[3].match(/^[0-9]+$/g))
+    {
+        const vmNum = parseInt(process.argv[3], 10);
+
+        if (config.config[vmNum])
+        {
+            const shutdown = commands.shutdown(config.config[vmNum].uuid).trim();
+
+            if (shutdown)
+            {
+                console.log(shutdown);
+            }
+        }
+    }
+    else
+    {
+        help(process.argv, ["shutdown"]);
+    }
 }
